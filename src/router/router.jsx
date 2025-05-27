ac9fcb5b4622b995c219643433b6252b0dc96505
mizanff <mrmizanurrahman626@gmail.com>
2025-05-27 10:02:10 +0600

    first commit

---
+++ src/router/router.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { createBrowserRouter } from "react-router";
+import RootLayouts from "../layouts/RootLayouts";
+import Home from "../pages/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayouts,
+    children: [
+        {
+            index: true,
+            Component: Home
+        }
+    ]
+  },
+]);
+
+export default router;
